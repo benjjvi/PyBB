9244e96d4b5ad1300903ebd638c74750034191e6
benjjvi <ben.voisey2@icloud.com>
2023-06-09 20:44:52 +0100

    fuck knows what i changed at this point

--- main.py
+++ main.py
@@ -7,23 +7,9 @@ app = Flask(__name__)
 def home():
     return render_template("base.html", title="default Jinja and Flask")
 
-students = [
-    {"name": "Sandrine",  "score": 100},
-    {"name": "Gergeley", "score": 87},
-    {"name": "Frieda", "score": 92},
-    {"name": "Fritz", "score": 40},
-    {"name": "Sirius", "score": 75},
-]
-
 @app.route("/results")
 def results():
-    context = {
-        "title": "Results",
-        "students": students,
-        "test_name": "a",
-        "max_score": "b",
-    }
-    return render_template("results.html", **context)
+    return render_template("base.html", title="test")
 
 if __name__ == "__main__":
     app.run(debug=True)
