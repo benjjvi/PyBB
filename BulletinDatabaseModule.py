1138a833a3f7a42f6b9b62863ac8a04cc3773604
benjjvi <ben.voisey2@icloud.com>
2023-06-10 14:38:06 +0100

    we have a prototype!

--- BulletinDatabaseModule.py
+++ BulletinDatabaseModule.py
@@ -1,78 +1,277 @@
 import sqlite3
 import cryptography
+from datetime import datetime
 
 # SQLite3 supports TEXT, INTEGER, REAL, BLOB and NULL only by default.
+# https://www.sqlite.org/datatype3.html
 
 class DB:
-    def __init__(self):
-        self.conn  = sqlite3.connect('db/users.db3')
-        self.log  = sqlite3.connect('db/log.db3')
+    def __init__(self, config):
+        self.config = config
+    
+    def connect(self):
+        return sqlite3.connect('db/bb.db3')
 
-        #Make start up entry.
-        c = self.log.cursor()
+    def get_boards(self):
+        db = self.connect() 
+        c = db.cursor()
+        c.execute("select * from boards")
 
-        # Create table
-        c.execute('''create table stocks
-        (date text, trans text, symbol text,
-        qty real, price real)''')
+        boards = c.fetchall()
 
-        # Insert a row of data
-        c.execute("""insert into stocks
-                values ('2006-01-05','BUY','RHAT',100,35.14)""")
-
-        # Save (commit) the changes
-        self.log.commit()
-
-        # We can also close the cursor if we are done with it
+        # Close cursor.
         c.close()
 
-    def write_log(self, log):
-        #Make start up entry.
-        c = self.log.cursor()
+        # Close connection.
+        db.close()
 
-        # Insert a row of data
-        c.execute("""insert into stocks
-                values ('2006-01-05','BUY','RHAT',100,35.14)""")
+        return boards
+    
+    def get_board_info(self, boardid):
+        db = self.connect() 
+        c = db.cursor()
+        c.execute(f"select * from boards where boardid = '{boardid}'")
 
-        # Save (commit) the changes
-        self.log.commit()
+        board = c.fetchall()
 
-        # We can also close the cursor if we are done with it
+        # Close cursor.
         c.close()
+
+        # Close connection.
+        db.close()
+
+        return board
+    
+    def get_posts_from_board(self, boardid):
+        db = self.connect() 
+        c = db.cursor()
+        c.execute(f"select * from posts where boardid = '{boardid}'")
+
+        posts = c.fetchall()
+
+        # Close cursor.
+        c.close()
+
+        # Close connection.
+        db.close()
+
+        return posts
+    
+    def get_post_info(self, postid):
+        db = self.connect() 
+        c = db.cursor()
+        c.execute(f"select * from posts where postid = '{postid}'")
+
+        post = c.fetchone()
+
+        # Close cursor.
+        c.close()
+
+        # Close connection.
+        db.close()
+
+        return post
+    
+    def get_comments_from_post(self, postid):
+        db = self.connect() 
+        c = db.cursor()
+        c.execute(f"select * from comments where postid = '{postid}'")
+
+        comments = c.fetchall()
+
+        # Close cursor.
+        c.close()
+
+        # Close connection.
+        db.close()
+
+        return comments
+    
+    def create_comment(self, postid, commentcontent, commentauthorid, commentauthorname, \
+                       commentdate=datetime.utcnow().strftime('%Y%m%d'), commenttime=datetime.utcnow().strftime('%H%M%S')):
+        db = self.connect()
+        c = db.cursor()
+
+        #Get total amount of comments on all posts, so we can add one to that and give this comment an ID
+        c.execute("select count(*) from comments")
+        commentid = c.fetchone()[0]+1
+
+        c.execute(f"""insert into comments
+                values ({postid}, {commentid}, '{commentcontent}', {commentauthorid}, '{commentauthorname}', {commentdate}, {commenttime})""")
+        
+        # Save (commit) the changes.
+        db.commit()
+
+        # Close cursor.
+        c.close()
+
+        # Close connection.
+        db.close()
+    
+    def create_board(self, boardid, boardtitle, boarddescription, boardicon):
+        db = self.connect() 
+        c = db.cursor()
+        c.execute(f"""insert into boards
+                values ({boardid}, '{boardtitle}', '{boarddescription}', '{boardicon}')""")
+
+        # Save (commit) the changes.
+        db.commit()
+
+        # Close cursor.
+        c.close()
+
+        # Close connection.
+        db.close()
+
+    def create_post(self, boardid, posttitle, postcontent, postauthorid, \
+                    postdate=datetime.utcnow().strftime('%Y%m%d'), posttime=datetime.utcnow().strftime('%H%M%S')):
+        db = self.connect() 
+        c = db.cursor()
+
+        #Get total amount of posts on all boards, so we can add one to that and give this post an ID
+        c.execute("select count(*) from posts")
+        postid = c.fetchone()[0]+1
+
+        c.execute(f"""insert into posts
+                values ({boardid}, {postid}, '{posttitle}', '{postcontent}', {postauthorid}, {postdate}, {posttime})""")
+
+        # Save (commit) the changes.
+        db.commit()
+
+        # Close cursor.
+        c.close()
+
+        # Close connection.
+        db.close()
+
+
+    def write_log(self, log_message):
+        #Make entry.
+        db = self.connect() 
+        c = db.cursor()
+
+        # Insert a row of data.
+        c.execute(f"""insert into log
+                values ({datetime.utcnow().strftime('%Y%m%d')}, {datetime.utcnow().strftime('%H%M%S')}, '{log_message}')""")
+
+        # Save (commit) the changes.
+        db.commit()
+
+        # Close cursor.
+        c.close()
+
+        # Close connection.
+        db.close()
 
     
-class Conf:
+class Configure:
     def __init__(self):
-        return NotImplemented
+        import ast
+        try:
+            f = open("pybb.conf", "r")
+            conf = f.read()
+            self.conf = ast.literal_eval(conf)
+            f.close()
+        except Exception as e:
+            self.reset_database()
+            self.__init__()
+
+    def get_config(self):
+        return self.conf
+            
+
+    def reset_database(self):
+        print("pybb.conf not found. Creating pybb.conf.")
+        with open("pybb.conf", "w") as f:
+            with open("defaults.conf", "r") as defaults:
+                f.write(defaults.read())
+        print("pybb.conf created. Please edit pybb.conf to your liking.")
+
+        print("Creating database files.")
+        self.create_database_files()
+        print("Database files created.")
+        
+        print("Setup complete. Please restart the program.")
     
     def create_database_files(self):
         import os
-        os.system("echo >> ./db/users.db3") # echo >> to be OS agnostic
-        os.system("echo >> ./db/log.db3")
-        os.system("echo >> ./db/board.db3")
+        os.system("echo >> ./db/bb.db3") # echo >> to be OS agnostic
         del os # try and increase security in program. if os is only available for milliseconds during the setup process alone,
             #     security of program is massively increased
 
         self.create_tables()
 
     def create_tables(self):
-        for db in ["users", "log", "boards"]:
-            tmp_conn  = sqlite3.connect(f'db/{db}.db3')
+        for db in ["users", "log", "boards", "posts", "comments"]:
+            tmp_conn  = sqlite3.connect(f'db/bb.db3')
 
             c = tmp_conn.cursor()
 
             if db == "users":
                 # Create table
                 c.execute(f'''create table {db}
-                (username text, password text, email text)''')
+                (userid integer, username text, hashedpassword text, email text, datejoined integer, locked integer)''')
             elif db == "log":
                 c.execute(f'''create table {db}
                 (date integer, time integer, log text)''') #follow structure 20231231 as 31st Dec 2023. time as 23:59
             elif db == "boards":
-                c.execute(f'''create table boards
-                (boardtitle text)''') 
+                c.execute(f'''create table {db}
+                (boardid integer, boardtitle text, boarddescription text, boardicon text)''') #boardicon is base64 encoded image.
+            elif db == "posts":
+                c.execute(f'''create table {db}
+                (boardid integer, postid integer, posttitle text, postcontent text, postauthorid integer, postdate integer, posttime integer)''')
+            elif db == "comments":
+                c.execute(f'''create table {db}
+                (postid integer, commentid integer, commentcontent text, commentauthorid integer, commentauthorname string, commentdate integer, commenttime integer)''')
 
             # closing cursor and connection to db
             tmp_conn.commit()
             c.close()
             tmp_conn.close()
+
+        self.fill_with_defaults()
+
+    def fill_with_defaults(self):
+        for db in ["users", "boards", "posts", "log", "comments"]:
+            tmp_conn  = sqlite3.connect(f'db/bb.db3')
+
+            c = tmp_conn.cursor()
+            if db == "users":
+                # Create table
+                c.execute(f"""insert into users
+                    values (0, 'admin', '{cryptography.get_hashed_password('admin').decode('utf-8')}', 'admin@pybb.net', {datetime.utcnow().strftime('%Y%m%d')}, 1)""")
+            elif db == "log":
+                c.execute(f"""insert into log
+                    values ({datetime.utcnow().strftime('%Y%m%d')}, {datetime.utcnow().strftime('%H%M%S')}, 'Created database files and populated them with tables.')""")
+            elif db == "boards":
+                c.execute("""insert into boards
+                    values (1, 'General', 'General discussion', '')""")
+            elif db == "posts":
+                #Get total amount of posts, so we can give a post ID to this post.
+                c.execute("select count(*) from posts")
+                postid = c.fetchone()[0]+1
+                c.execute(f"""insert into posts
+                    values (1, {postid}, 'Welcome to PyBB', 'Welcome to PyBB. This is a test post.', 0, {datetime.utcnow().strftime('%Y%m%d')}, {datetime.utcnow().strftime('%H%M%S')})""")
+            elif db == "comments":
+                c.execute(f"""insert into comments
+                    values (1, 1, 'This is a test comment.', 0, "admin", {datetime.utcnow().strftime('%Y%m%d')}, {datetime.utcnow().strftime('%H%M%S')})""")
+
+            tmp_conn.commit()
+            c.close()
+            tmp_conn.close()
+
+if __name__ == "__main__":
+    c = Configure()
+    db = DB(c.get_config())
+    db.write_log("Test log message.")
+    print(db.get_boards())
+    print(db.get_posts_from_board(1))
+    db.create_board(2, "Test", "Test board", "")
+    for i in range(0,100):
+        db.create_post(2, f"Test post {i}", f"Test post content {i}", 0)
+
+    for i in range(1,101):
+        for j in range(0,10):
+            db.create_comment(i, f"Test comment {j}", 0, "admin")
+    print(db.get_boards())
+    print(db.get_posts_from_board(2))
--- main.py
+++ main.py
@@ -1,15 +1,93 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import BulletinDatabaseModule
 
 app = Flask(__name__)
+Config = BulletinDatabaseModule.Configure()
+Database = BulletinDatabaseModule.DB(Config.get_config())
 
 @app.route("/")
 def home():
-    return render_template("base.html", title="default Jinja and Flask")
+    # Get the boards from the database:
+    boards = Database.get_boards()
+    
+    print(boards)
+    # Render the home page, with the boards:
+    return render_template("home.html", boards=boards)
 
-@app.route("/results")
-def results():
-    return render_template("base.html", title="test")
+
+#Using args
+
+#@app.route('/my-route')
+#def my_route():
+#  page = request.args.get('page', default = 1, type = int)
+#  filter = request.args.get('filter', default = '*', type = str)
+#/my-route?page=34               -> page: 34  filter: '*'
+#/my-route                       -> page:  1  filter: '*'
+#/my-route?page=10&filter=test   -> page: 10  filter: 'test'
+#/my-route?page=10&filter=10     -> page: 10  filter: '10'
+#/my-route?page=*&filter=*       -> page:  1  filter: '*'
+
+@app.route("/board")
+def boardView():
+    boardID = request.args.get('board', default = 1, type = int)
+    pageID= request.args.get('page', default = 1, type = int)
+
+    # Get the board information:
+    boardInfo = Database.get_board_info(boardID)
+
+    # Get the posts from the board:
+    posts = Database.get_posts_from_board(boardID)
+
+    # Reduce the list to 15 items (starting from the index specified by pageID).
+    posts = posts[(pageID-1)*15:pageID*15]
+
+    # Get the number of pages:
+    numberOfPages = len(Database.get_posts_from_board(boardID))//15+1
+
+    #debugging
+    print(f"boardID: {boardID}")
+    print(f"pageID: {pageID}")
+    print(f"boardInfo: {boardInfo}")
+    print(f"posts: {posts}")
+    print(f"numberOfPages: {numberOfPages}")
+
+    print(f"Type of boardID: {type(boardID)}")
+    print(f"Type of pageID: {type(pageID)}")
+    print(f"Type of boardInfo: {type(boardInfo)}")
+    print(f"Type of posts: {type(posts)}")
+    print(f"Type of numberOfPages: {type(numberOfPages)}")
+
+    print(f"Type of index0 of boardInfo: {type(boardInfo[0])}")
+    print(f"Type of index0 of posts: {type(posts[0])}")
+
+    print(f"Length of boardInfo: {len(boardInfo)}")
+    print(f"Length of posts: {len(posts)}")
+
+    print("==============")
+    for post in posts:
+        print(post)
+    print("==============")
+
+    return render_template("board.html", title=boardInfo[0][1], description=boardInfo[0][2], posts=posts, numberOfPages=numberOfPages, boardID=boardID, pageID=pageID)
+
+@app.route("/post")
+def postView():
+    # Get the post ID from the URL:
+    postID = request.args.get('post', default = 1, type = int)
+
+    # Get the post information:
+    postInfo = Database.get_post_info(postID)
+
+    # Get the comments from the post:
+    comments = Database.get_comments_from_post(postID)
+
+    #debugging
+    print(f"postID: {postID}")
+    print(f"postInfo: {postInfo}")
+    print(f"comments: {comments}")
+
+    return render_template("post.html", post=postInfo, comments=comments)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
